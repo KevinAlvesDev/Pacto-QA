d1ef82c6a591bf9307ad21699b08f8dccaaea4a5
Kevin Alves Dev <Kevinalveswp@gmail.com>
2025-08-07 00:43:11 -0300

    Finalizado

---
+++ Cypress-yahoo/cypress/e2e/yahoo_pacto.cy.js
@@ -0,0 +1,20 @@
+describe('Teste UOL - scroll até o final da página de termos', () => {
+  beforeEach(() => {
+    Cypress.on('uncaught:exception', () => false)
+  })
+
+  it('Acessa o site e scrolla até o final da página de termos', () => {
+    cy.visit('https://www.uol.com.br/', { timeout: 120000 })
+
+    cy.get('a[href="https://sobreuol.noticias.uol.com.br/normas-de-seguranca-e-privacidade.html"]', { timeout: 30000 })
+      .should('be.visible')
+      .click()
+
+    cy.origin('https://sobreuol.noticias.uol.com.br', () => {
+      cy.url({ timeout: 30000 }).should('include', 'normas-de-seguranca-e-privacidade')
+
+      // Scroll até o final da página
+      cy.scrollTo('bottom', { duration: 1000 })
+    })
+  })
+})
